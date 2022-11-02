18542749fa7ee929efd69db98fa3152438719ac3
swisskill <wbrant@uwyo.edu>
2022-11-02 09:38:14 -0600

    minChange

--- wargame/app/src/main/java/com/example/wargame/wargame.java
+++ wargame/app/src/main/java/com/example/wargame/wargame.java
@@ -10,6 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.Objects;
+
 public class wargame extends Fragment {
 
     public static wargame newInstance() {
@@ -20,7 +22,11 @@ public class wargame extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View myView = inflater.inflate(R.layout.fragment_wargame, container, false);
+        Objects.requireNonNull(((MainActivity) requireActivity()).getSupportActionBar()).hide();//get rid of toolbar
 
 
         return myView;
     } }
+/*
+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
+ */
