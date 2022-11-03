d871196dff07a6fb1c4407d13ebfe6041d611436
swisskill <wbrant@uwyo.edu>
2022-11-03 11:51:58 -0600

    win logic nearly works

--- wargame/app/src/main/java/com/example/wargame/wargame.java
+++ wargame/app/src/main/java/com/example/wargame/wargame.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.chip.Chip;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -21,10 +22,10 @@ public class wargame extends Fragment {
     public static wargame newInstance() {
         return new wargame();
     } //CONSTRUCTOR
-    ImageView xoro; //aka X or O; pronounced "zoro"
-    ImageView empty;
+    ImageView turnDisplay; //aka X or O; pronounced "zoro"
     Chip reset;
-    boolean turn = true; //where x is true and o is false; always start on x
+    int winner = 0; //x wins with 3, o wins with -3
+    int xoro = 1; //where x is 1 and o is -1
     int[][] gridMatrix = new int[3][3];
     //----------------------------------------------------------------------------------------------
     @Override
@@ -32,28 +33,74 @@ public class wargame extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         View myView = inflater.inflate(R.layout.fragment_wargame, container, false);
         Objects.requireNonNull(((MainActivity) requireActivity()).getSupportActionBar()).hide();//get rid of toolbar
-        xoro= myView.findViewById(R.id.imageView1);
+        turnDisplay= myView.findViewById(R.id.imageView1);
         reset=myView.findViewById(R.id.chip4);
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //reset the canvas and the matrix
+                gridMatrix = newInstance().gridMatrix;
             }
         });
         //upon click:
-            //draw image
+            //placeSymbol();
+            //checkWin();
             //turner();
 
         return myView;
     }
-    public void turner(){
-        if (turn) {  //if x
-            turn = false;
-            xoro.setImageResource(R.drawable.o);
-        } else {
-            turn = true;
-            xoro.setImageResource(R.drawable.x);
 
+
+    public void placeSymbol(){
+        if(xoro == 1){
+            //draw x where it should be
+        } else {
+            //draw o where it should be
+        }
+        //TODO: Implement matrix tracker
+        int col;
+        int row;
+        /*
+        whatever row was clicked, set row = rowClicked (int from 0-2)
+        whatever col was clicked, set col = colClicked (int from 0-2)
+        gridMatrix[row][col] = xoro //i believe this is the layout of the matrix
+
+        switch (placeClicked){ //use this if necessary
+            case 0<rowClicked<1:
+                row = 1;
+        }
+         */
+    }
+    public boolean checkWin(){ //this function scares me for the chance of bugs
+        boolean win = false;
+        //two individual loops for row and col are faster than a nested loop. Big O issue
+        for(int i = 0; i < 2; i++){
+            winner = gridMatrix[i][0] + gridMatrix[i][1] + gridMatrix[i][2];
+            win = checkCount();
+            if(win){break;}
+        }
+        for(int j = 0; j < 2; j++){
+            winner = gridMatrix[0][j] + gridMatrix[1][j] + gridMatrix[2][j];
+            win = checkCount();
+            if(win){break;}
+        }
+        return win;
+    }
+    public boolean checkCount(){
+        boolean win = false;
+        if(winner == 3){
+            win = true;
+        } else if (winner == -3){
+            win = true;
+        }
+        return win;
+    }
+    public void turner(){
+        if (xoro == 1) {  //if x
+            turnDisplay.setImageResource(R.drawable.o);
+            xoro = -1;
+        } else {
+            turnDisplay.setImageResource(R.drawable.x);
+            xoro = 1;
         }
     }
 }
